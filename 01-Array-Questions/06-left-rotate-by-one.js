762933abd191ad1a5259f85cee4a66df0b0b8ad9
Mohd Adnan <mohdadnan3508@gmail.com>
2025-08-13 16:46:42 +0530

    Left Rotate By One an array

---
+++ 01-Array-Questions/06-left-rotate-by-one.js
@@ -0,0 +1,17 @@
+function leftRotateByOne(arr, n) {
+    let temp = arr[0];
+
+    for(let i=1; i<n; i++) {
+        arr[i-1] = arr[i];
+    }
+
+    arr[n-1] = temp;
+
+    return arr;
+};
+
+
+let arr = [1, 2, 3, 4, 5];
+let n = arr.length;
+
+console.log(leftRotateByOne(arr, n));
