bc4f2cf19ec027c5a232e06b781d0c7acc4c3b86
Mohd Adnan <mohdadnan3508@gmail.com>
2025-08-13 15:58:27 +0530

    Check If Array Sorted Or Not

---
+++ 01-Array-Questions/04-isSorted.js
@@ -0,0 +1,16 @@
+function isSortedOrNot(arr) {
+
+    for(let i=1; i<arr.length; i++) {
+        if(arr[i] >= arr[i-1]) {
+
+        }else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let arr = [1, 2, 2, 3, 4, 5];
+
+console.log(isSortedOrNot(arr));
