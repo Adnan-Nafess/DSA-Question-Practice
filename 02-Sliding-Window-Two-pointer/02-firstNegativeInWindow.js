e4d318987add6a80ab32b6018f7ddf2eca96d64b
Mohd Adnan <mohdadnan3508@gmail.com>
2025-08-22 19:25:04 +0530

    first -ve integer in every windows of size k

--- 02-Sliding-Window-Two-pointer/02-firstNegativeInWindow.js
+++ 02-Sliding-Window-Two-pointer/02-firstNegativeInWindow.js
@@ -5,31 +5,36 @@ function firstNegativeInWindow(arr, k) {
   let result = [];
 
   while (end < arr.length) {
+    // Store negative numbers
     if (arr[end] < 0) {
       negatives.push(arr[end]);
     }
 
+    // Check if window size reached
     if (end - start + 1 === k) {
+      // Add first negative or 0
       if (negatives.length > 0) {
         result.push(negatives[0]);
       } else {
         result.push(0);
-
-        if (arr[start] === negatives[0]) {
-          negatives.shift();
-        }
-
-        start++;
       }
 
-      end++;
+      // Remove first element of window if it's the first negative
+      if (arr[start] === negatives[0]) {
+        negatives.shift();
+      }
+
+      // Slide the window
+      start++;
     }
 
-    return result;
+    end++;
   }
+
+  return result;
 }
 
 // Example
 let arr = [12, -1, -7, 8, -15, 30, 16, 28];
 let k = 3;
-console.log(firstNegativeInWindow(arr, k));
+console.log(firstNegativeInWindow(arr, k)); 
